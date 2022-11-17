aa3215219532cabb49bab9a5e382060c34b3a1e5
Humanz <109642842+OrochimaruBotz@users.noreply.github.com>
2022-11-17 19:39:09 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -17,19 +17,19 @@ global.autodltt = false
 global.autosticker = false
 global.autoreadsw = true
 global.ownerNomor = '6288804947282'
-global.ownerName = 'Creator Kayla'
+global.ownerName = 'Creator Sung Jinwoo'
 global.ownerNumber = ["6288804947282@s.whatsapp.net"]
 global.cek1 = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37','38','39','40','41','42','43','44','45','46','47','48','49','50','51','52','53','54','55','56','57','58','59','60','61','62','63','64','65','66','67','68','69','70','71','72','73','74','75','76','77','78','79','80','81','82','83','84','85','86','87','88','89','90','91','92','93','94','95','96','97','98','99','100']
 global.mess = {
-    wait: 'Wait Sis Please be patient',
-    succes: 'Good Luck Sis ?',
+    wait: 'Tunggu Kak Mohon bersabar.....*',
+    succes: 'Suksed',
     admin: 'Group Admin Special Features!!!',
     botAdmin: 'Bots Must Be Admins First!!!',
     owner: 'Ente Kadang-kadang Ente, Nih Khusus Owner Nih!!!',
     group: 'Features Used Only For Groups!!!',
     private: 'Features Used Only For Private Chat!!!',
     bot: 'Bot Number User Special Features!!!',
-    error: 'Error Sis, Please Chat Owner...',
+    error: '*Error, Please Chat Owner...*',
     premium: 'Maaf Sebelumya Kamu Belum Premium, Silahkan Pencet Di Bawah Untuk Beli Premium',
 }
 
